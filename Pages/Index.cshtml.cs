d76ed1d75867a054ab2794a4a0b744559add2f26
Gary <Gary.Dix@enform.nz>
2022-07-08 12:04:46 +1200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSD601_Counting_Letters_in_sentence.Pages
+{
+    [BindProperties]
+    public class IndexModel : PageModel
+    {
+        public string Text { get; set; }
+        public List<string> LetterCount { get; set; }
+
+        public IndexModel()
+        {
+            LetterCount = new List<string>();
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            CountLetters(Text);
+        }
+
+
+
+        private void CountLetters(string text)
+        {
+            var Letters = new Dictionary<char, int>();
+
+            //if there is no text use the sentance 
+            if (string.IsNullOrEmpty(text))
+            {
+                text = "if word is the word and bird is the word then word is the bird";
+            }
+
+            // for each letter in the word
+            foreach (char letter in text)
+            {
+                //if there is no entry in the dictionary then add a new entry with 1 as the counter
+                if (Letters.ContainsKey(letter) == false)
+                {
+                    Letters.Add(letter, 1);
+                }
+                else  // if its already in the dictionary increase the number (value) by one.
+                {
+                    //finds the key and adds 1 to the value, basically counts up
+                    Letters[letter]++;
+                }
+            }
+            //loop through the dictionary and get out the key/value pairs 
+
+            foreach (KeyValuePair<char, int> pair in Letters)
+            {
+                LetterCount.Add(pair.Key + " --> " + pair.Value);
+            }
+        }
+
+
+
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSD601_Counting_Letters_in_sentence.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
